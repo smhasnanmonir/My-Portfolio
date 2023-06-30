ffb134a3495724facf0c1e55c59955c492b59eb5
Hasnan <monirhasnan@gmail.com>
2023-06-30 23:56:43 +0600

    animation added

--- src/Components/Home/FirstPage/FirstPage.jsx
+++ src/Components/Home/FirstPage/FirstPage.jsx
@@ -1,22 +1,34 @@
+import { Bounce, Fade, Slide } from "react-awesome-reveal";
 import photo1 from "../../../assets/photo.png";
 const FirstPage = () => {
   return (
-    <div className="flex justify-center items-center md:px-0 px-[25px] md:flex-row flex-col-reverse">
+    <div className="flex justify-center items-center md:px-0 px-[25px] md:flex-row flex-col-reverse mt-[75px]">
       {/* Text Content div */}
       <div className="w-full space-y-[7px] md:mb-0 mt-[45px]">
-        <h1 className="text-3xl leading-[35px] font-rubic">Hi,</h1>
-        <h1 className="text-3xl leading-[35px] font-rubic">My Name is</h1>
-        <p className="text-[42px] text-[#f94949] font-googleFont font-semibold">
-          S.M Hasnan Monir
-        </p>
-        <p className="w-1/2 text-[18px] leading-[30px] font-rubic">
-          I'm a 22 years old CSE Student and learning web developing is my new
-          hobby.
-        </p>
+        <Fade cascade damping={0.5} duration={2000}>
+          <h1 className="text-3xl leading-[35px] font-rubic">Hi,</h1>
+          <h1 className="text-3xl leading-[35px] font-rubic">My Name is</h1>
+          <p className="text-[42px] text-[#f94949] font-googleFont font-semibold">
+            S.M Hasnan Monir
+          </p>
+          <p className="w-1/2 text-[18px] leading-[30px] font-rubic">
+            I'm a 22 years old CSE Student and learning web developing is my new
+            hobby.
+          </p>
+        </Fade>
       </div>
       {/* Photo div */}
       <div>
-        <img className="md:w-full" src={photo1} alt="My Image" />
+        <div className="md:block hidden">
+          <Slide damping={0.5} delay={2000}>
+            <img className="md:w-full" src={photo1} alt="My Image" />
+          </Slide>
+        </div>
+        <div className="md:hidden block">
+          <Slide damping={0.5}>
+            <img className="md:w-full" src={photo1} alt="My Image" />
+          </Slide>
+        </div>
       </div>
     </div>
   );
--- src/Components/Home/SkillsPage/SkillsPage.jsx
+++ src/Components/Home/SkillsPage/SkillsPage.jsx
@@ -6,6 +6,7 @@ import javascript from "../../../assets/logo-javascript.svg";
 import react from "../../../assets/react-2.svg";
 import node from "../../../assets/icons8-node-js.svg";
 import mongodb from "../../../assets/icons8-mongodb.svg";
+import { Fade } from "react-awesome-reveal";
 
 const SkillsPage = () => {
   return (
@@ -14,70 +15,72 @@ const SkillsPage = () => {
         My Skills
       </h1>
       <div className="grid md:grid-cols-4 gap-3 md:w-full place-items-center grid-cols-2 md-px-0 px-[25px]">
-        <div className="skill-div">
-          <img
-            className="w-[35px] mx-auto font-rubic"
-            src={html}
-            alt="html svg"
-          />
-          <p className="text-[18px] font-googleFont">HTML</p>
-        </div>
-        <div className="skill-div">
-          <img
-            className="w-[35px] mx-auto font-rubic"
-            src={css}
-            alt="css svg"
-          />
-          <p className="text-[18px] font-googleFont">CSS</p>
-        </div>
-        <div className="skill-div">
-          <img
-            className="w-[35px] mx-auto font-rubic"
-            src={bootstrap}
-            alt="bootstrap svg"
-          />
-          <p className="text-[18px] font-googleFont">BootStrap</p>
-        </div>
-        <div className="skill-div">
-          <img
-            className="w-[35px] mx-auto font-rubic"
-            src={tailwind}
-            alt="tailwind svg"
-          />
-          <p className="text-[18px] font-googleFont">Tailwind</p>
-        </div>
-        <div className="skill-div">
-          <img
-            className="w-[35px] mx-auto font-rubic"
-            src={javascript}
-            alt="javascript svg"
-          />
-          <p className="text-[18px] font-googleFont">JavaScript</p>
-        </div>
-        <div className="skill-div">
-          <img
-            className="w-[35px] mx-auto font-rubic"
-            src={react}
-            alt="html svg"
-          />
-          <p className="text-[18px] font-googleFont">React</p>
-        </div>
-        <div className="skill-div">
-          <img
-            className="w-[35px] mx-auto font-rubic"
-            src={node}
-            alt="Node svg"
-          />
-          <p className="text-[18px] font-googleFont">NodeJs</p>
-        </div>
-        <div className="skill-div">
-          <img
-            className="w-[35px] mx-auto font-rubic"
-            src={mongodb}
-            alt="Node svg"
-          />
-          <p className="text-[18px] font-googleFont">MongoDB</p>
-        </div>
+        <Fade cascade damping={0.1} duration={1500}>
+          <div className="skill-div">
+            <img
+              className="w-[35px] mx-auto font-rubic"
+              src={html}
+              alt="html svg"
+            />
+            <p className="text-[18px] font-googleFont">HTML</p>
+          </div>
+          <div className="skill-div">
+            <img
+              className="w-[35px] mx-auto font-rubic"
+              src={css}
+              alt="css svg"
+            />
+            <p className="text-[18px] font-googleFont">CSS</p>
+          </div>
+          <div className="skill-div">
+            <img
+              className="w-[35px] mx-auto font-rubic"
+              src={bootstrap}
+              alt="bootstrap svg"
+            />
+            <p className="text-[18px] font-googleFont">BootStrap</p>
+          </div>
+          <div className="skill-div">
+            <img
+              className="w-[35px] mx-auto font-rubic"
+              src={tailwind}
+              alt="tailwind svg"
+            />
+            <p className="text-[18px] font-googleFont">Tailwind</p>
+          </div>
+          <div className="skill-div">
+            <img
+              className="w-[35px] mx-auto font-rubic"
+              src={javascript}
+              alt="javascript svg"
+            />
+            <p className="text-[18px] font-googleFont">JavaScript</p>
+          </div>
+          <div className="skill-div">
+            <img
+              className="w-[35px] mx-auto font-rubic"
+              src={react}
+              alt="html svg"
+            />
+            <p className="text-[18px] font-googleFont">React</p>
+          </div>
+          <div className="skill-div">
+            <img
+              className="w-[35px] mx-auto font-rubic"
+              src={node}
+              alt="Node svg"
+            />
+            <p className="text-[18px] font-googleFont">NodeJs</p>
+          </div>
+          <div className="skill-div">
+            <img
+              className="w-[35px] mx-auto font-rubic"
+              src={mongodb}
+              alt="Node svg"
+            />
+            <p className="text-[18px] font-googleFont">MongoDB</p>
+          </div>
+        </Fade>
       </div>
     </div>
   );
